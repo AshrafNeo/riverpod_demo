5c9e593a2755c762f3fdf6a81846001191673e11
Mohdashraf Khan <ashraf.khan@neosoftmail.com>
2023-07-04 13:18:12 +0530

    Implemented observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_demo/observers.dart';
 
 import 'presentation/pages/todos/todos_list.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: MyApp(),
+    ProviderScope(
+      observers: [Observers()],
+      child: const MyApp(),
     ),
   );
 }
@@ -14,7 +16,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
